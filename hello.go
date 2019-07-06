9495361056321453263c07fe4f75e1c250625537
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-07-06 21:23:31 +0200

    refactor

--- hello.go
+++ hello.go
@@ -4,28 +4,34 @@ import "fmt"
 
 const spanish = "Spanish"
 const french = "French"
+const italian = "Italian"
 
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
 const frenchHelloPrefix = "Bonjour, "
+const italianHelloPrefix = "Ciao, "
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	case italian:
+		prefix = italianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
+}
 
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
-	prefix := englishHelloPrefix
-
-	switch language {
-	case french:
-		prefix = frenchHelloPrefix
-	case spanish:
-		prefix = spanishHelloPrefix
-	case "Italian":
-		prefix = "Ciao, "
-	}
-
-	return prefix + name
+	return greetingPrefix(language) + name
 }
 
 func main() {
