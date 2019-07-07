3e37f1223294cf4f529e0789e358a923a5f686c2
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-07-07 11:30:26 +0200

    added repeat char

---
+++ iteration/repeat_test.go
@@ -0,0 +1,40 @@
+package iteration
+
+import "testing"
+import "fmt"
+
+func TestRepeat(t *testing.T) {
+
+	assertRepeatCharacter := func(t *testing.T, repeated, expected string) {
+		if repeated != expected {
+			t.Errorf("expected '%s' but got '%s'", expected, repeated)
+		}
+	}
+
+	t.Run("repeat character a 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		assertRepeatCharacter(t, repeated, expected)
+	})
+
+	t.Run("repeat character a 7 times", func(t *testing.T) {
+		repeated := Repeat("a", 7)
+		expected := "aaaaaaa"
+
+		assertRepeatCharacter(t, repeated, expected)
+	})
+
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 4)
+	fmt.Println(repeated)
+	// Output: bbbb
+}
