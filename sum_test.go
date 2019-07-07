15b67a2857aa3fac4f31b0d62f8871818ace6671
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-07-07 18:14:17 +0200

    added SumAll

--- sum_test.go
+++ sum_test.go
@@ -1,10 +1,11 @@
 package main
 
 import "testing"
+import "reflect"
 
 func TestSum(t *testing.T) {
 
-	assertSum := func(t *testing.T, want, got int, numbers [5]int) {
+	assertSum := func(t *testing.T, want, got int, numbers []int) {
 		t.Helper()
 		if want != got {
 			t.Errorf("got %d want %d given, %v", got, want, numbers)
@@ -12,11 +13,29 @@ func TestSum(t *testing.T) {
 	}
 
 	t.Run("collection of 5 numbers", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+		numbers := []int{1, 2, 3, 4, 5}
 
 		got := Sum(numbers)
 		want := 15
 
 		assertSum(t, want, got, numbers)
 	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
+
+		got := Sum(numbers)
+		want := 6
+
+		assertSum(t, want, got, numbers)
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
