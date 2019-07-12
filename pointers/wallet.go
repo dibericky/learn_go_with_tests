83bd14c55619f423280bfbf7fb34decea642558a
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-07-12 23:10:33 +0200

    added pointers

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -1,13 +1,36 @@
 package pointers
 
-type Wallet struct {
-	balance int
+import (
+	"errors"
+	"fmt"
+)
+
+type Bitcoin int
+
+// Implementation of the Stringer interface, defined in fmt, used when print with %s
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
 
-func (w *Wallet) Deposit(amount int) {
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+var ErrInsufficientFunds = errors.New("cannot withdraw, insufficient funds")
+
+func (w *Wallet) Withdraw(amount Bitcoin) error {
+	if amount > w.balance {
+		return ErrInsufficientFunds
+	}
+
+	w.balance -= amount
+	return nil
 }
