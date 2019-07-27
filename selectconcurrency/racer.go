a1f9c50d80222db4a1b0203ee5737ad5513649d4
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2019-07-27 18:05:21 +0200

    added select

--- selectconcurrency/racer.go
+++ selectconcurrency/racer.go
@@ -1,7 +1,9 @@
 package selectconcurrency
 
 import (
+	"fmt"
 	"net/http"
+	"time"
 )
 
 func ping(url string) chan bool {
@@ -13,11 +15,19 @@ func ping(url string) chan bool {
 	return ch
 }
 
-func Racer(a, b string) (winner string) {
+var tenSecondTimeout = 10 * time.Second
+
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, err error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
+}
+
+func Racer(a, b string) (winner string, err error) {
+	return ConfigurableRacer(a, b, tenSecondTimeout)
 }
